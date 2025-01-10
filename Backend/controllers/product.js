d4c413b36cc381c21c7644f381b7ccda2f6f3a05
haslan82 <haslan82@hotmail.com>
2025-01-10 23:42:51 +0300

    update

--- Backend/controllers/product.js
+++ Backend/controllers/product.js
@@ -1,27 +1,27 @@
-const product = require("../models/product");
-const Product = require("../models/product");
+/* const product = require("../models/product");
+const ProductFilter = require("../models/product");
 
-const allProducts = async (res, req) => {
-    const productFilter = await Product.find();
-  const products = await Product.find();
-  res.status(200).json(products);
+const allProducts = async (req, res) => {
+    const productFilter = new ProductFilter(Product.find(), req.query);
+  const products = await 
+  res.status(200).json({products});
 };
 
 
 
 
-const detailProducts = async (res, req) => {
-  const products = await Product.findById(req.params.id);
-  res.status(200).json(products);
+const detailProducts = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  res.status(200).json({product});
 };
 
-const createProducts = async (res, req) => {
-  const products = await Product.create(req.body);
-  res.status(201).json(products);
+const createProducts = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.status(201).json(product);
 };
 
-const deleteProducts = async (res, req) => {
-  const products = await Product.findById(req.params.id);
+const deleteProducts = async (req, res) => {
+  const product = await Product.findById(req.params.id);
 
   product.removeAllListeners();
 
@@ -30,8 +30,8 @@ const deleteProducts = async (res, req) => {
   });
 };
 
-const updateProducts = async (res, req) => {
-  const products = await Product.findById(req.params.id);
+const updateProducts = async (req, res) => {
+  const product = await Product.findById(req.params.id);
 
   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -40,6 +40,80 @@ const updateProducts = async (res, req) => {
   res.status(200).json({
     product,
   });
+};
+
+module.exports = {
+  allProducts,
+  detailProducts,
+  createProducts,
+  deleteProducts,
+  updateProducts,
+}; */
+
+
+const Product = require("../models/product");
+const ProductFilter = require("../models/product");
+
+const allProducts = async (req, res) => {
+  try {
+    const productFilter = new ProductFilter(await Product.find(), req.query);
+    res.status(200).json({ products: productFilter });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const detailProducts = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Ürün bulunamadı." });
+    }
+    res.status(200).json({ product });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createProducts = async (req, res) => {
+  try {
+    if (!req.body.name || !req.body.price) {
+      return res.status(400).json({ message: "Ürün adı ve fiyatı gerekli!" });
+    }
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteProducts = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Ürün bulunamadı." });
+    }
+    await product.remove();
+    res.status(200).json({ message: "Ürün başarıyla silindi." });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateProducts = async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Ürün bulunamadı." });
+    }
+    res.status(200).json({ product: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = {
